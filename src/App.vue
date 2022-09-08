95fbbc73d51588e8df7cc49635442aa99e8de186
Schwaldi <sebastianschwald@gmail.com>
2022-09-08 15:16:22 +0200

    add todo list logic

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,78 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="bg-gray-600 text-white w-1/2 m-auto mt-12 rounded">
+    <div class="text-center border-b-2 border-black py-4">
+      <h1 class="text-3xl py-4">Unsere Todos</h1>
+      <p class="text-xl" v-if="openTodos.length > 0">
+        Offene Todos:{{ openTodos.length }}
+      </p>
+      <p class="text-xl" v-else>
+        There are no Todos, congratulation!
+      </p>
+      <div class="mt-4 px-2">
+        <input type="test" class="py-2 w-2/3 text-black" v-model="newTodo"/>
+        <button class="bg-red-400 py-2 w-1/3" @click="addTodo">Add Todo</button>
+      </div>
+    </div>
+    <div v-for="(todo, index) in todos" :key="todo.todo">
+      <Todo 
+        :todoprop="todo" 
+        :todoindex="index" 
+        @toggledone-index="toggleDone"
+        @removetodo-index="deleteTodo"
+      />
+    </div>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Todo from './components/Todo.vue'
 
 export default {
   name: 'App',
+  data() {
+    return {
+      newTodo: "",
+      todos: [],
+    };
+  },
+  methods: {
+    toggleDone(index) {
+      this.todos[index].done =!this.todos[index].done;
+      this.storeTodos();
+    },
+    deleteTodo(index) {
+      this.todos.splice(index, 1);
+      this.storeTodos();
+    },
+    addTodo() {
+      if(this.newTodo.trim() === "") {
+        return;
+      }
+      this.todos.push({todo: this.newTodo, done: false });
+      this.storeTodos();
+    },
+    storeTodos() {
+      localStorage.setItem("todos", JSON.stringify(this.todos));
+    },
+  },
+  mounted () {
+    let data = localStorage.getItem("todos");
+    if(data !== "" && data !=null) {
+      this.todos = JSON.parse(data);
+    } else {
+      this.todos = [];
+    }
+  },
+  computed: {
+    openTodos (){
+      const openTodos = this.todos.filter ((todo) => {
+        return !todo.done;
+      });
+      return openTodos;
+    },
+  },
   components: {
-    HelloWorld
-  }
+    Todo,
+  },
 }
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
---
+++ src/components/Todo.vue
@@ -0,0 +1,29 @@
+<template>
+  <div 
+    class="flex justify-between items-center py-2 px-2" 
+    :class="{ 'bg-green-600':this.todo.done, 'bg-red-600': !todo.todo.done }"
+  >
+    <h1 class="cursor-pointer" @click="toggleTodo">{{ todo.todo }}</h1>
+    <button class="bg-black py-1 px-2 rounded" @click="removeTodo">Delete Todo</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Todo-vue',
+  props: ["todoprop","todoindex"],
+  data() {
+    return {
+      todo: this.todoprop,
+    }
+  },
+  methods: {
+    toggleTodo() {
+      this.$emit("toggledone-index", this.todoindex);
+    },
+    removeTodo() {
+      this.$emit("removetodo-index", this.todoindex);
+    },
+  },
+}
+</script>
